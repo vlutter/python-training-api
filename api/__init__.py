31130269307fa73e68db9ec90188b1b2fb6abc4a
Vladislav Dmitriev <vladislav.dmitriev@netcracker.com>
2024-05-27 19:21:26 +0300

    Discard sqlite env

--- api/__init__.py
+++ api/__init__.py
@@ -3,15 +3,11 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///python-training.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
